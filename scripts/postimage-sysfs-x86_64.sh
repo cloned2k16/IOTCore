1a5ada7b26d298b96af01a1a13c7a6b3d2081fb7
botfap <botfap@gmail.com>
2017-01-16 02:05:09 +0000

    V0.9.0 - Migration to uClibc-ng

--- scripts/postimage-sysfs-x86_64.sh
+++ scripts/postimage-sysfs-x86_64.sh
@@ -5,8 +5,7 @@ rm -rf ../output-images/sys*
 
 cp output/images/rootfs.tar.gz ../output-images/sysfs.tgz
 cp output/images/bzImage ../output-images/sysk
-cp output/images/rootfs.cpio ../output-images/sysrd
-cp output/images/rootfs.iso9660 ../output-images/recovery.iso
+cp output/images/rootfs.cpio.gz ../output-images/sysrd
 cp output/images/rootfs.squashfs ../output-images/sysfsq
 
 #rm -rf output/images/*
